899610281fa8b565961d2904e0a74e2392c15899
LukaszChojnowski246 <chojnowskilukasz9@gmail.com>
2025-10-22 15:43:25 +0200

    Add files via upload

---
+++ .py
@@ -0,0 +1,29 @@
+#x = 4
+
+#if x % 2 == 0:
+#    print("liczba parzysta")
+#else:
+#    print("licza nie jest parzysta")
+
+#_____________________________________________________
+
+#x = int(input("podaj wiek: "))
+
+#if x < 18:
+#
+#    print("jestes dzieciak")
+#else:
+#    print("jestes dorosly")
+
+
+#_______________________________________________________
+
+
+
+
+
+
+
+
+
+
